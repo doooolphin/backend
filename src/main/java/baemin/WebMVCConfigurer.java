c69a53dcb980f802afb5e666c4076289f3d55786
ohi2262 <ohi2262@naver.com>
2023-03-11 22:24:57 +0900

    feature/ swagger setting

---
+++ src/main/java/baemin/WebMVCConfigurer.java
@@ -0,0 +1,15 @@
+package baemin;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/4.17.1/");
+    }
+}
